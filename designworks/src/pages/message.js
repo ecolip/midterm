cbd5b47195bd14c1a9c1f1f755c20eb806f4b12a
Emma Cheng <ecolipp@gmail.com>
2022-06-11 17:44:26 +0800

    Add message page.

---
+++ designworks/src/pages/message.js
@@ -0,0 +1,94 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import api from '../utils/api';
+
+const Container = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`;
+const Title = styled.table`
+  padding-top: 32px;
+  font-size: 32px;
+  padding-bottom: 32px;
+  font-weight: bold;
+`;
+const Table = styled.table`
+  width: 100%;
+  border:1px solid black;
+`;
+const Tr = styled.tr`
+`;
+const Thead = styled.thead`
+`;
+const Tbody = styled.tbody`
+`;
+const Th = styled.th`
+  border:1px solid black;
+`;
+const Td = styled.td`
+  cursor: ${(props)=> props.isCursor ? 'pointer' : 'none'};
+  background-color: ${(props)=> props.isCursor ? 'black' : 'none'};
+  color: ${(props)=> props.isCursor ? 'white' : 'black'};
+  border-radios: ${(props)=> props.isCursor ? '2px' : 'none'};
+
+  text-align: center;
+  border:1px solid black;
+`;
+
+function Message() {
+  const [message, setMessage] = useState(null);
+
+  const getMessage = () => {
+    api.getMessages().then((res) => {
+      setMessage(res);
+    })
+  };
+
+  const RemoveMessage = (id) => {
+    api.deleteMessage(id).then((res) => {
+      getMessage();
+    })
+  };
+
+  const renderMessages = () => {
+    const output = message.map((item) => {
+      return (
+        <Tr key={'message- '+ item.id}>
+          <Td>{item.name}</Td>
+          <Td>{item.email}</Td>
+          <Td>{item.message}</Td>
+          <Td>{item.timestamp}</Td>
+          <Td isCursor onClick={()=>{RemoveMessage(item.id)}}>Delete</Td>
+        </Tr>
+      )
+    } );
+    return output;
+  };
+
+  useEffect(()=>{
+    getMessage();
+  }, [])
+
+
+  return (
+    <Container>
+      <Title>Message List</Title>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th>Email</Th>
+            <Th>Message</Th>
+            <Th>Timestamp</Th>
+            <Th>Delete</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {message && renderMessages()}
+        </Tbody>
+      </Table>
+    </Container>
+  );
+}
+
+export default Message;
--- designworks/src/utils/api.js
+++ designworks/src/utils/api.js
@@ -1,5 +1,10 @@
 const api = {
   hostname: 'http://localhost:3000',
+  getMessages() {
+    return fetch(`${this.hostname}/messages`).then(
+      (response) => response.json()
+    );
+  },
   sendMessage(data) {
     return fetch(`${this.hostname}/messages`, {
       body: JSON.stringify(data),
@@ -7,6 +12,14 @@ const api = {
         'Content-Type': 'application/json',
       }),
       method: 'POST',
+    }).then((response) => response.json());
+  },
+  deleteMessage(id) {
+    return fetch(`${this.hostname}/messages/${id}`, {
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+      method: 'DELETE',
     }).then((response) => response.json());
   },
 };
